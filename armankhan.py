dbcf7b8322e453f0fad48a6ff0bb2909b270e3bf
armankhan2k56-creator <armankhan2k56@gmail.com>
2026-08-13 20:33:18 +0500

    Update armankhan.py
    rm -rf armankhan
    git clone https://github.com/armankhan2k56-creator/armankhan.git
    cd armankhan
    python armankhan.py

--- armankhan.py
+++ armankhan.py
@@ -121,10 +121,9 @@ def send_login_alert(user_key, user_name, expiry_date):
     )
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": TELEGRAM_USER, "text": message}
-    try:
-                try:
-                   requests.post(url, data=payload, timeout=5)
-               except Exception:
+          try:
+        requests.post(url, data=payload, timeout=5)
+    except Exception:
         pass
     if expiry_str:
         exp_dt = datetime.strptime(expiry_str, "%Y-%m-%d %H:%M")
