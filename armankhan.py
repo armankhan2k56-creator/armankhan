3742ae75e380b00cc32712681a40dbae4ceaa81e
armankhan2k56-creator <armankhan2k56@gmail.com>
2026-08-20 11:31:32 +0500

    Update armankhan.py
    cd ~
    rm -rf armankhan
    git clone https://github.com/armankhan2k56-creator/armankhan.git
    cd armankhan
    python armankhan.py

--- armankhan.py
+++ armankhan.py
@@ -161,7 +161,7 @@ def Public():
                 'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
                 'viewport-width': '980',
             }
-                ken=open(".token.txt","w").write(find_token)
+		    open(".token.txt","w").write(find_token)
                 print (" [] Login Successful")
                 login()
                 except Exception as e:
