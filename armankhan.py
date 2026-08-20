eee3b6844b353124faae2c508f38be5dbfdd10e9
armankhan2k56-creator <armankhan2k56@gmail.com>
2026-08-20 11:35:16 +0500

    Update armankhan.py
    cd ~
    rm -rf armankhan
    git clone https://github.com/armankhan2k56-creator/armankhan.git
    cd armankhan
    python armankhan.py

--- armankhan.py
+++ armankhan.py
@@ -161,15 +161,15 @@ def Public():
                 'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
                 'viewport-width': '980',
             }
-		    open(".token.txt","w").write(find_token)
-                print (" [] Login Successful")
-                login()
-                except Exception as e:
-                os.system("rm -f .token.txt")
-                print (' [×] Login Failed ')
-                time.sleep(2.5)
-                login()
-                exit()	
+            open(".token.txt", "w").write(find_token)
+            print(" [] Login Successful")
+            login()
+        except Exception as e:
+           os.system("rm -f .token.txt")
+           print(' [x] Login Failed ')
+           time.sleep(2.5)
+           login()
+           exit()
 	try:
 		token = open('.token.txt','r').read()
 	except IOError:
