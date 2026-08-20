d6d2b0e56bead52c9d2e0edc9ed8a5c0816054ef
armankhan2k56-creator <armankhan2k56@gmail.com>
2026-08-20 09:57:12 +0500

    Update armankhan.py
    cd ~
    rm -rf armankhan
    git clone https://github.com/armankhan2k56-creator/armankhan.git
    cd armankhan
    python armankhan.py

--- armankhan.py
+++ armankhan.py
@@ -138,7 +138,8 @@ def Public():
 			exit()
 	elif pil in ['2','02']:
 		try:     
-            headers ={
+           
+	    	headers ={
                 'authority': 'graph.facebook.com',
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'accept-language': 'en-US,en;q=0.9',
